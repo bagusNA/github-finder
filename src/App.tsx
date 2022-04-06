61a3e2290ea960f3ed7a371e6acdc1e9f4bef906
Bagus Nur A <bagusnur@protonmail.com>
2022-04-06 14:49:57 +0800

    Added search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React from "react";
-import { IoSunnyOutline, IoLocation, IoLink, IoMail, IoBusiness } from 'react-icons/io5';
+import React, { useEffect, useState } from "react";
+import { IoSunnyOutline, IoLocation, IoLink, IoMail, IoBusiness, IoLogoTwitter } from 'react-icons/io5';
+import Moment from 'react-moment';
 
-import SearchBar from './components/SearchBar';
+import { SearchBar } from './components/SearchBar';
 import { StatEntry } from './components/StatEntry';
 import { FieldEntry } from "./components/FieldEntry";
 import defaultProfile from './assets/img/profile.jpg';
@@ -12,6 +13,38 @@ const settings = {
 }
 
 function App() {
+  const [userData, setUserData] = useState({
+    name: 'Patrick Star',
+    login: 'patrick',
+    html_url: 'https://github.com/users/bagusNA',
+    avatar_url: 'https://avatars.githubusercontent.com/u/84782823?v=4',
+    createdAt: '2021-05-25T12:38:17Z',
+    email: 'patrickstar@mail.com',
+    company: 'Rock Company',
+    location: 'Bikini Bottom',
+    twitter_username: 'patrickOnTwit',
+    public_repos: 7,
+    followers: 2,
+    following: 7,
+    isDefault: true,
+    bio: null,
+    blog: null,
+  });
+  const [search, setSearch] = useState('');
+
+  function handleSearchChange(e: any) {
+    setSearch(e.target.value);
+  }
+
+  async function handleSearchAction() {
+    setUserData(await getUserData(search))
+  }
+
+  function getUserData(username: string) {
+    return fetch('https://api.github.com/users/' + username)
+      .then(res => res.json())
+  }
+
   return (
     <main className="min-h-screen flex justify-center bg-dark text-sm text-light font-mono lg:text-base">
       <div className="min-h-screen flex flex-col gap-y-6  p-6 md:justify-center md:container lg:max-w-screen-lg">
@@ -25,14 +58,14 @@ function App() {
         </div>
 
         {/* Search bar */}
-        <SearchBar />
+        <SearchBar value={search} onInputChange={handleSearchChange} onButtonClick={handleSearchAction} />
 
         {/* Main card */}
         <div className="bg-card-dark p-5 rounded-2xl shadow-2xl lg:p-8">
           <div className="flex gap-x-12 ">
             {/* Profile picture for lg screen */}
               <img 
-                src={defaultProfile}
+                src={userData.avatar_url}
                 alt="Default profile"
                 width={settings.lgPictureWidth}
                 className="rounded-full hidden self-start lg:block"
@@ -40,37 +73,44 @@ function App() {
             <div className="flex flex-col gap-y-6">
               {/* Main info */}
               <div className="flex gap-x-6">
-                <img src={defaultProfile} alt="Default profile" className="rounded-full lg:hidden" width={settings.pictureWidth} />
+                <img src={userData.avatar_url} alt="Default profile" className="rounded-full lg:hidden" width={settings.pictureWidth} />
                 <div className="flex flex-col justify-center gap-y-1 lg:flex-row lg:justify-between lg:w-full">
                   <div className="flex flex-col justify-center gap-y-1">
-                    <h2 className="font-bold text-xl">Bagus Nur A.</h2>
+                    <h2 className="font-bold text-xl">{userData.name}</h2>
                     <a 
                       className="text-blue-400" 
-                      href="https://github.com/bagusNA"
+                      href={userData.html_url}
                     >
-                      @bagusNA
+                      @{userData.login}
                     </a>
                   </div>
-                  <p className="">Joined 7 July, 2019</p>
+                  <p>Joined at <Moment format="MMMM DD, YYYY" date={Date.parse(userData.createdAt)} /></p>
                 </div>
               </div>
 
                 {/* Description */}
-              <p className="leading-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit delectus voluptatibus error quidem enim! Officia atque vero repellat nulla recusandae rem! Modi mollitia minus placeat quod minima fugit itaque voluptatem!</p>
+              <p className="leading-8">
+                {userData.bio
+                  ? userData.bio 
+                  : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit delectus voluptatibus error quidem enim! Officia atque vero repellat nulla recusandae rem! Modi mollitia minus placeat quod minima fugit itaque voluptatem!'}
+              </p>
 
               {/* Profile stats */}
               <div className="grid grid-cols-3 justify-between py-4 bg-dark rounded-xl md:px-8">
-                <StatEntry title='Repos' value={8} />
-                <StatEntry title='Followers' value={8} />
-                <StatEntry title='Following' value={8} />
+                <StatEntry title='Repos' value={userData.public_repos} />
+                <StatEntry title='Followers' value={userData.followers} />
+                <StatEntry title='Following' value={userData.following} />
               </div>
 
               {/* Fields */}
               <div className="grid grid-cols-1 gap-3 md:grid-cols-2">
-                <FieldEntry icon={<IoLocation />} value='East Borneo, Indonesia' />
-                <FieldEntry icon={<IoLink />} value='https://github.com/bagusNA' href="https://github.com/bagusNA"/>
-                <FieldEntry icon={<IoMail />} value='bagusnur@protonmail.com' href="mailto:bagusnur@protonmail.com"/>
-                <FieldEntry icon={<IoBusiness />} value='Intern at Jupiter IT Solution' />
+                <FieldEntry icon={<IoLocation />} value={userData.location} />
+                <FieldEntry icon={<IoLink />} 
+                  value={userData.blog ? userData.blog : userData.html_url} 
+                  href={userData.blog ? userData.blog : userData.html_url} 
+                />
+                <FieldEntry icon={<IoLogoTwitter />} value={userData.twitter_username} href={`https://twitter.com/${userData}`}/>
+                <FieldEntry icon={<IoBusiness />} value={userData.company} />
               </div>
             </div>
           </div>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { IoSearch } from 'react-icons/io5';
 
-export default function SearchBar() {
+interface Props {
+  value: string,
+  onButtonClick: any,
+  onInputChange: any
+}  
+
+export const SearchBar:React.FC<Props> = props => {
   return (
     <div className="relative flex justify-between px-2 py-2 bg-card-dark rounded-2xl shadow-2xl md:text-lg">
       <label htmlFor="searchInput" className="absolute inset-y-0 left-0 flex items-center pl-3 md:pl-4">
@@ -11,10 +17,13 @@ export default function SearchBar() {
         type="text" 
         placeholder="Search GitHub username" 
         id="searchInput"
+        value={props.value}
+        onChange={props.onInputChange}
         className="pl-9 flex-1 bg-transparent appearance-none outline-none truncate md:pl-14"
       />
       <button 
         className="bg-blue-600 px-4 py-3 font-bold rounded-lg transition shadow-xl hover:bg-blue-500"
+        onClick={props.onButtonClick}
       >
         Search
       </button>
