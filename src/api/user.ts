d44c3a5d8131b683664479d8fa0e0356f468de4b
bagusnur <bagusnur@protonmail.com>
2022-06-02 16:25:34 +0800

    per page option in search

--- src/api/user.ts
+++ src/api/user.ts
@@ -3,8 +3,8 @@ const getUserData = async (username: string) => {
     .then(res => res.json());
 }
 
-const searchUser = async (username: string) => {
-  return await fetch(`https://api.github.com/search/users?q=${username}`)
+const searchUser = async (username: string, perPage: number = 10) => {
+  return await fetch(`https://api.github.com/search/users?q=${username}&per_page=${perPage}`)
   .then(res => res.json());
 }
 
