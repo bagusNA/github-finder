c5dedf1d81921788884889f477d74c551579d425
bagusnur <bagusnur@protonmail.com>
2022-06-02 16:17:09 +0800

    SearchCard now uses proper interface

--- src/components/cards/SearchCard.tsx
+++ src/components/cards/SearchCard.tsx
@@ -1,31 +1,30 @@
 import React from 'react';
+import { User } from "../../types/User";
 
 interface Props {
-  username: string,
-  avatar: string,
-  name?: string,
+  user: User,
   onClick?: any
 }
 
-const SearchCard:React.FC<Props> = (props) => {
+const SearchCard:React.FC<Props> = ({user, onClick}) => {
   return (
     <div 
       className="flex bg-card-light rounded-2xl shadow-2xl overflow-hidden transition select-none dark:bg-card-dark dark:hover:bg-card-dark-hover hover:cursor-pointer"
-      onClick={props.onClick}
+      onClick={onClick}
     >
       <img 
-        src={props.avatar} 
+        src={user.avatar_url} 
         alt="Default profile" 
         className="h-auto w-24 lg:w-32" 
       />
       <div className="flex flex-col justify-center pl-8 py-4">
-        {props.name ?
+        {user.name ?
           <React.Fragment>
-            <p className="text-xl">{props.name}</p>
-            <p className="opacity-90 hover:opacity-100 text-blue-400">/{props.username}</p>
+            <p className="text-xl">{user.name}</p>
+            <p className="opacity-90 hover:opacity-100 text-blue-400">/{user.login}</p>
           </React.Fragment>
           :
-          <p className="opacity-100 text-blue-400 text-xl">/{props.username}</p>
+          <p className="opacity-100 text-blue-400 text-xl">/{user.name}</p>
         }
       </div>
     </div>
